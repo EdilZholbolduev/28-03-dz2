2924584871da3ddff010f1e334c669204d5afca0
Edil Zholbolduev <tashtan841@gmail.com>
2023-07-21 18:23:22 +0200

    text will  occure on display (web)

--- dz 4 traffic light/app.js
+++ dz 4 traffic light/app.js
@@ -1,16 +1,17 @@
 function trafLight(){ 
    var trafficLight = document.querySelectorAll('.light')
    console.log(trafficLight)
+   var text = document.querySelector('.rule')
    var color = prompt('enter a trafficlight color')
    if(color === 'red' || color === 'Red'){
       trafficLight[0].style.backgroundColor = 'red'
-      console.log('STOP!')
+      text.textContent = 'STOP!'
    }else if(color === 'yellow' || color === 'Yellow'){
       trafficLight[1].style.backgroundColor = 'yellow'
-      console.log('Be ready to go!')
+      text.textContent = 'BE READY!'
    }else if(color === 'green' || color === 'Green'){
       trafficLight[2].style.backgroundColor = 'green'
-      console.log('Goo!!')
+      text.textContent = 'GO!'
    }else{
       alert('404 not found (enter only trafficLight colors)')
    }
